1dc73bd5cddfa4d2a3c515cbd07b24266392f48f
AssetGrav <417051@mail.ru>
2024-03-03 22:49:48 +0600

    get request

--- api/index.js
+++ api/index.js
@@ -1,3 +1,38 @@
-import app from "./index";
+const express = require("express");
+const config = require("config");
+const cors = require("cors");
+const chalk = require("chalk");
 
-export default app;
+const routes = require("../routes");
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+
+// app.get("/api", async (req, res) => {
+//   res.send("Hello World!");
+// });
+
+// app.post("/api", async (req, res) => {
+//   const { name } = req.query;
+//   console.log("1586", name);
+//   res.send(name);
+// });
+
+app.use("/api", routes);
+
+const PORT = config.get("port") ?? 8080;
+
+// if (process.env.NODE_ENV === 'production') {
+//     console.log('Production')
+// } else {
+//     console.log('Development')
+// }
+
+app.listen(PORT, () => {
+  console.log(chalk.green(`Server has been started on port ${PORT}...`));
+});
+
+module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const { sendMessage } = require("../telegraf");
 const router = express.Router({ mergeParams: true });
 
 router.use("/bot", require("./bot.router"));
+
+router.get("/", async (req, res) => {
+  const { name, phone } = req.query;
+
+  const getInfo = await sendMessage(
+    `My name ${name}, phone ${"https://wa.me/8" + phone}`
+  );
+
+  return res.send(getInfo);
+});
 
 module.exports = router;
